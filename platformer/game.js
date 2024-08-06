dac4debd5017f2688b1e8996d24d21eebb1b0087
Tani Aura <111664369+taniwha3@users.noreply.github.com>
2024-08-05 21:27:25 -0700

    attempt to fix level list
    Signed-off-by: Tani Aura <111664369+taniwha3@users.noreply.github.com>

--- platformer/game.js
+++ platformer/game.js
@@ -487,13 +487,13 @@ class StarParticle {
     }
 }
 
-import { level1 } from '../levels/level1.js';
-import { level2 } from '../levels/level2.js';
-import { level3 } from '../levels/level3.js';
-import { level4 } from '../levels/level4.js';
-import { level5 } from '../levels/level5.js';
-import { level6 } from '../levels/level6.js';
-import { level7 } from '../levels/level7.js';
+import { level1 } from 'levels/level1.js';
+import { level2 } from 'levels/level2.js';
+import { level3 } from 'levels/level3.js';
+import { level4 } from 'levels/level4.js';
+import { level5 } from 'levels/level5.js';
+import { level6 } from 'levels/level6.js';
+import { level7 } from 'levels/level7.js';
 
 const levels = [level1, level2, level3, level4, level5, level6, level7];
 
